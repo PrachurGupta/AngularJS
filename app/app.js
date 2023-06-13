384e67e6a7d500586b98a64b7de2099b5b5b92bc
Prachur Gupta <prachur12@gmail.com>
2023-06-13 17:40:48 +0530

    L14 ng-submit

--- app/app.js
+++ app/app.js
@@ -3,36 +3,49 @@ var myNinjaApp = angular.module("myNinjaApp", []);
 myNinjaApp.controller("NinjaController", [
   "$scope",
   function ($scope) {
+    $scope.removeNinja = function (ninja) {
+      var removedNinja =
+        $scope.ninjas.indexOf(ninja);
+      $scope.ninjas.splice(removedNinja, 1);
+    };
 
-    $scope.removeNinja = function(ninja){
-        var removedNinja = $scope.ninjas.indexOf(ninja);
-        $scope.ninjas.splice(removedNinja, 1);
-    }
+    $scope.addNinja = function(){
+        $scope.ninjas.push({
+            name: $scope.newninja.name,
+            belt: $scope.newninja.belt,
+            rate: parseInt($scope.newninja.rate),
+            available: true
+        });
+
+        $scope.newninja.name = "";
+        $scope.newninja.belt = "";
+        $scope.newninja.rate = "";
+    };
 
     $scope.ninjas = [
       {
         name: "Yoshi",
         belt: "Green",
         rate: 50,
-        available: true
+        available: true,
       },
       {
         name: "Crystal",
         belt: "Yellow",
         rate: 30,
-        available: true
+        available: true,
       },
       {
         name: "Ryu",
         belt: "Orange",
         rate: 10,
-        available: false
+        available: false,
       },
       {
         name: "Shaun",
         belt: "Black",
         rate: 1000,
-        available: true
+        available: true,
       },
     ];
   },
